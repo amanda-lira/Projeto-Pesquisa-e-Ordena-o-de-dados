f454e756530f8b7a263f86c0b450db7b411bf6cd
VictorNicolau-coder <victor.estudo1575@gmail.com>
2026-06-20 16:14:58 -0300

    style: muda estrutura do projeto

--- algoritimos/ordenacao/merge_sort.py
+++ algoritimos/ordenacao/merge_sort.py
@@ -17,42 +17,23 @@ def merge_sort(arr):
 
         while i < len(metade_esquerda) and j < len(metade_direita):
             comparacoes += 1
-<<<<<<< HEAD
             if metade_esquerda[i] <= metade_direita[j]:
-=======
-            if metade_esquerda[i] < metade_direita[j]:
->>>>>>> 23ad691 (feat (Validacao): implementa os calculos de metricas utilizadas nos graficos para validar o seletor, bem como o arquivo gerar_graficos.py)
                 arr[k] = metade_esquerda[i]
                 i += 1
             else:
                 arr[k] = metade_direita[j]
                 j += 1
-<<<<<<< HEAD
                 trocas += (len(metade_esquerda) - i)
             k += 1
-=======
-            k += 1
-            trocas += 1 # Cada atribuição a arr[k] pode ser considerada uma troca em termos de movimentação de dados
->>>>>>> 23ad691 (feat (Validacao): implementa os calculos de metricas utilizadas nos graficos para validar o seletor, bem como o arquivo gerar_graficos.py)
 
         while i < len(metade_esquerda):
             arr[k] = metade_esquerda[i]
             i += 1
             k += 1
-<<<<<<< HEAD
-=======
-            trocas += 1
->>>>>>> 23ad691 (feat (Validacao): implementa os calculos de metricas utilizadas nos graficos para validar o seletor, bem como o arquivo gerar_graficos.py)
 
         while j < len(metade_direita):
             arr[k] = metade_direita[j]
             j += 1
             k += 1
-<<<<<<< HEAD
 
     return arr, comparacoes, trocas
-=======
-            trocas += 1
-
-    return arr, comparacoes, trocas
->>>>>>> 23ad691 (feat (Validacao): implementa os calculos de metricas utilizadas nos graficos para validar o seletor, bem como o arquivo gerar_graficos.py)
